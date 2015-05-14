d1989a5a3431a1f37ec9627352c4e354027d5743
Robert Herget <RHerget_H@dev.bse.edu>
2015-05-14 17:18:29 -0500

    Got the basic maintenance working

--- functions.js
+++ functions.js
@@ -1,51 +1,26 @@
-$(document).ready(function(){
+var data = [];
+var id = 0;//id is just
 
-	function dropdown(){
-		var opt = "<select> -- Choose -- </select>";
-		$(opt).wrap("<opt />");
-		$("#catalog").html(opt);
-	}
-	dropdown();
-	function room(){}//define the various rooms give names
-
-	function box(){}
-
-	function item(){}
-
-	function createnew(){
-		function room(){}
-		function box(){}
-		function item(){}
-	}
-
-	function deletse(){
-		function room(){}
-		function box(){}
-		function item(){}
-	}
-
-	function database(){}//chose database to store
-
-	function search(){}//define search function
-
-	function move(){}
-
-	function description(){}
-
-	function previousinfo(){
-		function getfromdb(){}
-		//do stuff
-	}
-
-	function setname(){
-		//room
-		//box
-		//item
-	}
-
-});
+function newthing(){
+	var dataitem = {//this var takes in my input values and stores them to array data
+	id: id,//could also use data.length to keep track of id
+	parentid: $("#field4").val(),//number of the thing that it is in
+	name: $("#field1").val(),
+	body: $("#field2").val(),
+	Areas: $("#field3").val()
+};
+	id++;//increments my id
+	console.log(id);//shows id
+	(data).push(dataitem);//actually puts dataitem in data
+	console.log("this works");
+	console.log(dataitem);
+	//console.log(data);
+	return false;
+}
+//loop through arrays and show things, list everything in a dropdown from the array
+//parent id should be shown as dropdown
 
 
-
-
-
+// $(document).ready(function(){
+	
+// });
