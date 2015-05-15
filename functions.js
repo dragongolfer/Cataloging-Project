22557bf2878eae21ab2ea31e5b4d4401e05ca03c
Robert Herget <RHerget_H@dev.bse.edu>
2015-05-15 16:17:55 -0500

    I can add things currently, I am working on browse now

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 var data = [];
 var id = 0;//id is just
+console.log(data.name);
+console.log(id);
 
 function newthing(){
 	var dataitem = {//this var takes in my input values and stores them to array data
@@ -13,10 +15,25 @@ function newthing(){
 	console.log(id);//shows id
 	(data).push(dataitem);//actually puts dataitem in data
 	console.log("this works");
-	console.log(dataitem);
+	console.log(data + "this works/start");
+	
 	//console.log(data);
 	return false;
+	//$.each(data, function )
 }
+
+function browsething(){
+	for(var i=0; i < data.length; i++){
+		console.log(i + data[i]);
+	}
+	console.log(data[i]);
+	return false;
+}
+//console.log($(data).get[0]);
+
+//function xxxxx(){
+//	for data.item in data 
+//}
 //loop through arrays and show things, list everything in a dropdown from the array
 //parent id should be shown as dropdown
 
